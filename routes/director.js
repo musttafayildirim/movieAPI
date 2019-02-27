b3541ab9d30f202584e1d7befb5f9b14a81e5484
Mustafa YILDIRIM <mustafa.yildirim1997@gmail.com>
2019-02-27 14:39:06 +0300

    gelen director id'ye göre filmlerini döndürme

--- routes/director.js
+++ routes/director.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 
 
@@ -16,6 +17,109 @@ router.post('/', (req, res) => {
  })
 });
 
+
+router.get('/', (req, res) =>{
+    const promise = Director.aggregate([
+        {
+            $lookup: {
+                from : 'movies',
+                localField : '_id',
+                foreignField : 'director_id',
+                as : 'movie'
+            }
+        },
+        {
+            $unwind : {
+                path : '$movie',
+                //herhangi bir datayla eşleşmese bile veriyi döndürmeyi sağlayan sorgu....
+                preserveNullAndEmptyArrays : true
+
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname:  '$surname',
+                    bio : '$bio'
+                },
+                movie :{
+                    $push : '$movie'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                movie : '$movie'
+            }
+        }
+
+    ]);
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+
+router.get('/:director_id', (req, res) =>{
+    const promise = Director.aggregate([
+        {
+            $match: {
+                '_id' : mongoose.Types.ObjectId(req.params.director_id)
+            }
+        },
+
+        {
+            $lookup: {
+                from : 'movies',
+                localField : '_id',
+                foreignField : 'director_id',
+                as : 'movie'
+            }
+        },
+        {
+            $unwind : {
+                path : '$movie',
+                //herhangi bir datayla eşleşmese bile veriyi döndürmeyi sağlayan sorgu....
+                preserveNullAndEmptyArrays : true
+
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname:  '$surname',
+                    bio : '$bio'
+                },
+                movie :{
+                    $push : '$movie'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                movie : '$movie'
+            }
+        }
+
+    ]);
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
 
 
 
